2200ef030b1f488e339c098fb1ba39dd66bd1a1c
JinsoRaj <jinso.trv18it032@gecbh.ac.in>
2021-12-29 00:04:21 +0530

    upto 6

--- main.js
+++ main.js
@@ -4,8 +4,30 @@ const app = Vue.createApp({
             product: 'Socks',
             description: 'A warm fuzzy pair of socks.',
             image: './assets/images/socks_green.jpg',
-            url: 'https://www.vuemastery.com/'
+            url: 'https://jinsoraj.github.io/VueApp/',
+            inStock: false,
+            inventory: 100,
+            details: ['50% cotton', '30% wool', '20% poly'],
+            varients: [
+                { id: 123, color: 'green', image: './assets/images/socks_green.jpg'},
+                { id: 124, color: 'blue', image: './assets/images/socks_blue.jpg'}
+            ],
+            sizes: ['S', 'M', 'L', 'XL'],
+            cart: 0
             
         }
     },
+    methods: {
+        addToCart(){
+            this.cart += 1
+        },
+        removeFromCart() {
+            if (this.cart >= 1) {
+                this.cart -= 1
+            }
+        },
+        updateImage(varientImage){
+            this.image= varientImage
+        }
+    }
 })

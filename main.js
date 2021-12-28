dc237897abe250924cfa50673d2ce8f86225e204
JinsoRaj <jinso.trv18it032@gecbh.ac.in>
2021-12-28 22:34:40 +0530

    test

---
+++ main.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Socks',
+            description: 'A warm fuzzy pair of socks.',
+            image: './assets/images/socks_green.jpg',
+            url: 'https://www.vuemastery.com/'
+            
+        }
+    },
+})
